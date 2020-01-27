11f82883c2325bee25ff321a1878d202203d1781
Lourdes Lorenzana <lourdeslorenochoa@gmail.com>
2020-01-27 10:12:56 -0600

    trabajando

---
+++ Tarea1_201602935/src/tarea1_201602935/Tarea1_201602935.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1_201602935;
+
+/**
+ *
+ * @author lourd
+ */
+public class Tarea1_201602935 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Prueba");
+    }
+    
+}
