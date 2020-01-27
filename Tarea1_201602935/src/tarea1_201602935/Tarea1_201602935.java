ee0556c7855e890dd494aac0afddda2931c8dd99
Lourdes Lorenzana <lourdeslorenochoa@gmail.com>
2020-01-27 10:55:40 -0600

    terminado

--- Tarea1_201602935/src/tarea1_201602935/Tarea1_201602935.java
+++ Tarea1_201602935/src/tarea1_201602935/Tarea1_201602935.java
@@ -5,6 +5,8 @@
  */
 package tarea1_201602935;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lourd
@@ -16,7 +18,23 @@ public class Tarea1_201602935 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Prueba");
+        int M;
+        Scanner m = new Scanner(System.in);
+        System.out.println("Ingresar valor de m");
+        M = m.nextInt();
+        String[][] matriz = new String[M][M];
+        
+        for( int i = 0; i < M; i++ ) {
+            for( int j = 0; j < M; j++ ) {
+                if( i==0 || i==M-1 || j==0 || j==M-1 )
+                    System.out.print("1");
+                else
+                     System.out.print(" ");
+             }
+             System.out.println(" ");
+        }
+        
+        
     }
     
 }
