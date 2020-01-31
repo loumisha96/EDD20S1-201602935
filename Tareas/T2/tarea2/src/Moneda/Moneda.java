9ce7c4ba22daa74277e339d304938a132ada4838
Lourdes Lorenzana <lourdeslorenochoa@gmail.com>
2020-01-31 13:21:31 -0600

    metodos hechos

--- Tareas/T2/tarea2/src/Moneda/Moneda.java
+++ Tareas/T2/tarea2/src/Moneda/Moneda.java
@@ -10,22 +10,84 @@ package Moneda;
  * @author lourd
  */
 public class Moneda{
+      public String nombre;
+      public int valor;
+      public static int cantMonedero;
+      public int IDdueño;
+      public static int[][] matriz = new int[5][2];
+
+      
       
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    public Moneda(){
-        String nombre;
-      int valor;
-    }
-    public void Crear(){
+        for(int i=0; i<5; i++){
+            matriz[i][0]=i;
+        }
+        Moneda mon = new Moneda("dsg",3);
+        mon.CrearMoneda("quetzal", 1);
+        mon.CrearMoneda("quetzal", 2);
+        mon.CrearMoneda("cinco", 5);
+        mon.CrearMoneda("diez", 10);
+        System.out.println("Monedas en el monedero= " + cantMonedero);
+        mon.TransferirFrom(1, 0, 5);
+        mon.Transferir(0,5);
+        mon.TransferirFrom(0, 1, 5);
         
     }
-    public void Transferir(){
+    public Moneda(String nombre, int valor){
+        this.nombre = nombre;
+        this.valor = valor;
         
     }
-}
+    public void CrearMoneda(String nombre, int valor){
+        Moneda moneda = new Moneda(nombre, valor);
+        cantMonedero = cantMonedero+valor;
+        
+    }
+    public void Transferir(int IDdueño, int cant ){
+        if(cantMonedero< cant){
+            System.out.println("No hay monedas");
+        }else{
+            
+                for (int i = 0; i<5; i++){
+                    if((IDdueño == matriz[i][0])){
+                        matriz[i][1] = matriz[i][1]+cant;
+                        cantMonedero--;
+                        System.out.println("El Usuario "+IDdueño+ " tiene " +matriz[i][1]+ " monedas");
+                        i=5;
+                    }
+                }
+            
+        }
+        
+    }
+    //dueño1 transfiere a dueño2
+    public void TransferirFrom(int IDdueño1, int IDdueño2, int cant){
+        for(int i = 0; i<5; i ++){
+            if(matriz[i][0]== IDdueño1){
+                if(matriz[i][1]<cant){
+                    System.out.println("El usuario " +IDdueño+ "no tiene monedas para transferir");
+                    i=5;
+                }
+                else{
+                    matriz[i][1]=matriz[i][1]-cant;
+                    for(int j=0; j<5; j++){
+                        if(matriz[j][0] ==IDdueño2){
+                            matriz[j][1]=matriz[j][1]+cant;
+                            System.out.println("Moneda recibida");
+                            j=5;
+                        }
+                    }
+                    i=5;
+                }
+            }
+        }
+        
+        
+    }
+    
 
+}
---
+++ Tareas/T2/tarea2/src/Moneda/T2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Moneda;
+
+/**
+ *
+ * @author lourd
+ */
+public class T2 {
+    Moneda Moneda = new Moneda("moneda", 3);
+     /*  Moneda.CrearMoneda("quetzal", 1);
+        CrearMoneda("cinco", 5);
+        CrearMoneda("quetzal", 1);
+        CrearMoneda("diez", 10);
+        CrearMoneda("quetzal", 1);
+        System.out.println("Monedas en el monedero= " + cantMonedero);*/
+}
