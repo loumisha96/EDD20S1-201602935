14c348aa2e8e6d2dc68c311bb473aedec2fae96e
Lourdes Lorenzana <lourdeslorenochoa@gmail.com>
2020-02-01 22:45:04 -0600

    terminado

--- Tareas/T2/tarea2/src/Moneda/Moneda.java
+++ Tareas/T2/tarea2/src/Moneda/Moneda.java
@@ -17,26 +17,27 @@ public class Moneda{
       public static int cantMonedero;
       public int IDdueño;
       public int cantUsuario;
-      public static int[][] matriz;
-
+      public static String[][] matriz;
+      public static int k;//tamaño de la matriz
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+      
+      public static void main(String[] args) {
         // TODO code application logic here
         Scanner dueño = new Scanner(System.in);
-        int nombre;
-        int j;
-        System.out.println("Ingresar cantidad de dueños");
-        j = dueño.nextInt();
+        String nombre;
         
-        matriz = new int[j][3];
-        for(int i=0; i<j; i++){
+        System.out.println("Ingresar cantidad de dueños");
+        k = dueño.nextInt();
+        
+        matriz = new String[k][3];
+        for(int i=0; i<k; i++){
             System.out.println("Ingresar nombre del dueño");
-            nombre = dueño.nextInt();
-            matriz[i][0]= i;
-            matriz[i][1]= 3;
-            matriz[i][2]= 0;
+            nombre = dueño.next();
+            matriz[i][0]= String.valueOf(i);
+            matriz[i][1]= nombre;
+            matriz[i][2]= String.valueOf(0);
         }
         Moneda mon = new Moneda("dsg",3);
         mon.CrearMoneda("quetzal", 1);
@@ -44,16 +45,13 @@ public class Moneda{
         mon.CrearMoneda("cinco", 5);
         mon.CrearMoneda("diez", 10);
         System.out.println("Monedas en el monedero= " + cantMonedero);
-        mon.TransferirFrom(1, 0, 5);
-        mon.Transferir(0,5);
-        mon.TransferirFrom(0, 1, 5);
+        mon.TransferirFrom("1", "2", 5);
+        mon.Transferir("2",5);
+        mon.TransferirFrom("2", "0", 5);
         System.out.println("Monedas en el monedero= " + cantMonedero);
         
     }
-    //se crean con 0 monedas
-    public void AgregarUsuario(String nombre){
-      //  matriz[][] =  
-    }
+    //se crean  monedas
     public Moneda(String nombre, int valor){
         this.nombre = nombre;
         this.valor = valor;
@@ -65,16 +63,17 @@ public class Moneda{
         
     }
     //del monedero al dueño
-    public void Transferir(int IDdueño, int cant ){
+    //Parámetro de entrada es el ID del dueño
+    public void Transferir(String IDdueño, int cant ){
         if(cantMonedero< cant){
             System.out.println("No hay monedas");
         }else{
             
                 for (int i = 0; i<5; i++){
-                    if((IDdueño == matriz[i][0])){
+                    if((IDdueño.equals( matriz[i][0]))){
                         matriz[i][2] = matriz[i][2]+cant;
                         cantMonedero = cantMonedero - cant;
-                        System.out.println("El Usuario "+IDdueño+ " tiene " +matriz[i][2]+ " monedas");
+                        System.out.println(matriz[i][1]+ " tiene " +matriz[i][2]+ " monedas");
                         i=5;
                     }
                 }
@@ -83,19 +82,24 @@ public class Moneda{
         
     }
     //dueño1 transfiere a dueño2
-    public void TransferirFrom(int IDdueño1, int IDdueño2, int cant){
-        for(int i = 0; i<5; i ++){
-            if(matriz[i][0]== IDdueño1){
-                if(matriz[i][2]<cant){
-                    System.out.println("El usuario " +IDdueño+ " no tiene monedas para transferir");
+    //Los parámetros de entrada son los ID de los dueños
+    public void TransferirFrom(String IDdueño1, String IDdueño2, int cant){
+        for(int i = 0; i<k; i ++){
+            if(matriz[i][0].equals(IDdueño1)){
+                if(Integer.parseInt(matriz[i][2])<cant){
+                    System.out.println(matriz[i][1]+ " no tiene monedas para transferir");
                     i=5;
                 }
                 else{
-                    matriz[i][2]=matriz[i][2]-cant;
+                    int mon = Integer.parseInt(matriz[i][2]);
+                    matriz[i][2]=String.valueOf(mon-cant);
                     for(int j=0; j<5; j++){
-                        if(matriz[j][0] ==IDdueño2){
-                            matriz[j][2]=matriz[j][2]+cant;
-                            System.out.println("Moneda recibida");
+                        int mon1 = Integer.parseInt(matriz[j][2]);
+                        matriz[j][2]=String.valueOf(mon1-cant);
+                        
+                        if(matriz[j][0].equals(IDdueño2)){
+                            matriz[j][2]=String.valueOf(mon1+cant);
+                            System.out.println(matriz[j][1]+ " recibió " +cant + " monedas");
                             j=5;
                         }
                     }
