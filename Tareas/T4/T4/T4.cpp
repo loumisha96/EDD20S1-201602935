8b2c033586ce962bdcf44204b61f9f06b96b36fc
Lourdes Lorenzana <lourdeslorenochoa@gmail.com>
2020-02-11 16:31:32 -0600

    terminado

--- Tareas/T4/T4/T4.cpp
+++ Tareas/T4/T4/T4.cpp
@@ -3,10 +3,7 @@
 #include <iostream>
 
 using namespace std;
-int main()
-{
-    std::cout << "Hello World!\n"; 
-}
+
 class nodo {
 public:
 	char letra;
@@ -28,13 +25,13 @@ public:
 	Lista() {
 		this->ultimo = 0;
 		this->primero = 0;
-		
+
 	}
 	bool vacia() {
-		primero = 0;
+		return primero == 0;
 	}
 	//insertar al inicio
-	void insertar(char letra) {
+	void insertarPrimero(char letra) {
 		nodo* nuevo = new nodo(letra);
 		if (vacia()) {
 			primero = nuevo;
@@ -42,11 +39,114 @@ public:
 			tam = 0;
 		}
 		else {
-			ultimo->sig = nuevo;
-			nuevo->ant = ultimo;
-			ultimo = nuevo;
+			primero->ant = nuevo;
+			nuevo->sig = primero;
+			primero = nuevo;
 			tam++;
 		}
 	}
+	void eliminarUltimo() {
+		ultimo->ant->sig = 0;
+		ultimo->ant = ultimo;
+		ultimo->sig = 0;
+		tam--;
+	}
+	int posinicio = -1;
+	int posfinal = -1;
+	nodo* buscar(string s, nodo*aux1, int cont) {
+		bool var = true;
+		nodo *aux = aux1;
+		
+		int cont1=cont;
+		while (var) {
+			for (auto x : s) {
 
+				if (aux->letra == x) {
+					cont1++;
+					if (posinicio < 0)
+						posinicio = cont1;
+					if (aux->sig== 0 || aux->letra == ' ')
+						posfinal = cont1;
+
+					aux = aux->sig;
+				}
+				else {
+					while (aux->sig != 0) {
+						if (aux->letra != ' ') {
+							aux = aux->sig;
+							cont1++;
+							
+						}
+
+						else {
+							aux = aux->sig;
+							//cont++;
+							buscar(s, aux, cont1);
+							break;
+						}
+						
+						//break;
+					}
+
+					if (aux->sig == 0) {
+						var = false;
+
+					}
+					//break;
+				}
+			}
+			var = false;
+			aux = primero;
+			for (int i = 0; i < tam + 1; i++) {
+				if (posinicio == i) {
+					return aux;
+				}
+				else {
+					if (aux->sig != 0)
+						aux = aux->sig;
+
+				}
+			}
+		}
+	}
+	void sd(string s, nodo* aux1, int cont) {
+		
+		nodo *s1 = buscar(s, aux1, cont);
+		if (cont> 0) {
+			cout << "Valor encontrado: ";
+			cout << s1->letra << endl;
+		}
+		else {
+			cout << "No se encuentra coincidencia ";
+		}
+		
+	}
+
+	void print() {
+		nodo* aux = primero;
+		while (aux->sig != 0) {
+			cout << aux->letra << endl;
+			aux = aux->sig;
+		}
+		cout << aux->letra << endl;
+		
+	}
 };
+int main()
+{
+	Lista* lista = new Lista();
+	lista->insertarPrimero('M');
+	lista->insertarPrimero(' ');
+	lista->insertarPrimero('A');
+	lista->insertarPrimero('L');
+	lista->insertarPrimero('O');
+	lista->insertarPrimero('H');
+	nodo* aux = lista->primero;
+	int cont = -1;
+	lista->sd("A", aux, cont);
+	//lista->buscar("M", aux, cont);
+
+	
+	
+}
+
